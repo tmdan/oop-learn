8e0b40144b1a3e9a5848bb6717699bcc8a91a2ef
Didar <nepesov7212@mail.ru>
2020-03-22 12:48:25 +0300

    я сделал домашку но не понел для чего пользоваться геттером здесь

---
+++ didar/2-task/index.php
@@ -0,0 +1,54 @@
+<?php
+class Student
+{
+    private $name;
+    private $secondname;
+    private $scholarship;
+    private $salary;
+
+    public function setname($name)
+    {
+    $this->name = $name;
+    }
+    public function getname()
+    {
+     return $this->name;
+    }
+    public function setsecondname($secondn)
+    {
+        $this->secondname = $secondn;
+    }
+    public function  getsecondname()
+    {
+        return $this->secondname;
+    }
+    public function setscholarship($scholars)
+    {
+        $this->scholarship=$scholars;
+    }
+    public function setsolary($solary)
+    {
+        $this->salary = $solary;
+    }
+    public function getFullName()
+    {
+        echo $this->name . $this->secondname;
+    }
+    public function calculate()
+    {
+        echo $this->scholarship + $this->salary;
+    }
+
+}
+
+$firststudent = new Student();
+
+$firststudent->setname(Didar );
+$firststudent->setsecondname(Nepesow);
+$firststudent->setscholarship(100);
+$firststudent->setsolary(150);
+$firststudent->getFullName();
+echo "<br>";
+$firststudent->calculate();
+
+?>
