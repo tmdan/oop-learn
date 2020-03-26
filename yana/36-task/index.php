00672d308e1df58a8da009c56295cbbcdda1a1ff
Яна <andreiocsana@gmail.com>
2020-03-26 23:03:34 +0300

    Домашка 36 исправлена

--- yana/36-task/index.php
+++ yana/36-task/index.php
@@ -4,12 +4,21 @@ $names=["Мухаммед","Яна", "Дидар", "Севинч", "Иса"];
 $lastNames=["Зеленский", "Пупкина", "Лукашенко", "Ельцин", "Путин"];
 $uni=["БГТУ","БГМУ", "БНТУ", "БГАТУ"];
 $fac=["Экономический","Физический", "Юридический", "Химический", "Биологический"];
+$pred=["Физика", "Химия", "Математика", "Биология", "Физкультура"];
+$gr=[1, 2, 3, 5, 9, 10];
 
 $students=[];
+
+class subject {
+    public $name;
+    public $grade;
+}
+
 
 class univer{
     public $name;
     public $facult;
+    public $subjects;
 }
 
 
@@ -20,10 +29,15 @@ class student{
 }
 
 for($i=0;$i<count($names);$i++){
-    $students[$i]=new student();
+    $students[$i]=new student;
     $students[$i]->name=$names[$i];
     $students[$i]->lName=$lastNames[$i];
-    $students[$i]->univ=new univer();
-    $students[$i]->univ->name=$uni[$i];
-    $students[$i]->univ->facult=$fac[$i];
+    $students[$i]->univ=new univer;
+    $students[$i]->univ->name=$uni[rand(0,5)];
+    $students[$i]->univ->facult=$fac[rand(0,5)];
+    for($j=0;$j<3;$j++) {
+        $students[$i]->univ->subjects[$j] = new subject;
+        $students[$i]->univ->subject[$j]->name = $pred[rand(0, 5)];
+        $students[$i]->univ->subject[$j]->grade = $gr[rand(0, 5)];
+    }
 }
