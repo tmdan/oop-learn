95a89aab57b5f8bf740aabe803b05f94e994bea8
tmdan <mukhammed.ne@gmail.com>
2020-03-29 23:55:50 +0300

    38 занятие примеры которые были на занятии

---
+++ muhammed/array_functions/index.php
@@ -0,0 +1,13 @@
+
+<h1>Функции для работы с массивами</h1>
+
+<ul>
+    <li><a href="array_map-numbers.php"><b>array_map</b> - перебор всех элементов массива - числа</a></li>
+    <li><a href="array_map-objects.php"><b>array_map</b> - перебор всех элементов массива - объекты</a></li>
+    <li><a href="array_filter-numbers.php"><b>array_map</b> - фильтрация всех элементов массива - числа</a></li>
+    <li><a href="array_filter-objects.php"><b>array_map</b> - фильтрация всех элементов массива - объекты</a></li>
+</ul>
+
+
+
+
