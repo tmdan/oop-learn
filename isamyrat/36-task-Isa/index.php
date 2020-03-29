66f9a71d791c081e3beb510787c83e2d88a342e0
admin <kurbanowisa07@gmail.com>
2020-03-29 17:37:40 +0300

    I do my hm

---
+++ isamyrat/36-task-Isa/index.php
@@ -0,0 +1,60 @@
+<?php
+
+$names = ["Isa", "Yana", "Didar"];
+$surnames = ["Ivanow", "Petrov", "Sidorov"];
+$nameofunivers = ["BNTU", "BGU", "BGUIR"];
+$nameoffacs = ["IEF", "FITU", "KCIC"];
+$predmets_name = ["OOP", "KC", "TRPP"];
+$persons = [];
+
+class person
+{
+    public $name;
+    public $surname;
+    public $univ;
+    public function calc()
+    {
+    }
+}
+
+class Univer
+{
+    public $nameofuniver;
+    public $nameoffac;
+    public $predmets;
+}
+
+class predmet
+{
+    public $name_pred;
+    public $value_pred;
+}
+
+for ($i = 0; $i < count($names); $i++) {
+    $persons[$i] = new person();
+    $persons[$i]->name = $names[rand(0, 3)];
+    $persons[$i]->surname = $surnames[rand(0, 3)];
+    $persons[$i]->univ = new  Univer();
+    $persons[$i]->univ->nameofuniver = $nameofunivers[rand(0, 3)];
+    $persons[$i]->univ->nameoffac = $nameoffacs[rand(0, 3)];
+    for ($n = 0; $n < count($predmets_name); $n++) {
+        $persons[$i]->univ->predmets[$n] = new predmet;
+        $persons[$i]->univ->predmets[$n]->name_pred = $predmets_name[$n];
+        $persons[$i]->univ->predmets[$n]->value_pred = rand(3, 10);
+    }
+}
+for ($i = 0; $i < count($persons); $i++) {
+    echo $persons[$i]->name;
+    echo $persons[$i]->surname;
+    echo $persons[$i]->univ->nameofuniver;
+    echo $persons[$i]->univ->nameoffac;
+    for ($n = 0; $n < count($persons); $n++) {
+        echo $persons[$i]->univ->predmets[$n]->name_pred;
+        echo $persons[$i]->univ->predmets[$n]->value_pred;
+        echo "<br>";
+        echo "средняя оценка";
+        echo ($persons[$i]->univ->predmets[$n]->value_pred+($persons[$i]->univ->predmets[$n]+1)->value_pred+($persons[$i]->univ->predmets[$n]+2)->value_pred)/3;
+    }
+
+}
+
