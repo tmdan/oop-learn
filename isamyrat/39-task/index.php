bf7f275bbe76bb7e97125bc7f7317ea472aab61f
admin <kurbanowisa07@gmail.com>
2020-04-05 15:05:48 +0300

    remove hm

--- isamyrat/39-task/index.php
+++ isamyrat/39-task/index.php
@@ -1,53 +1,80 @@
 <?php
+
+
 class Engine
 {
     public $oil;
 
     public function __construct($oil)
     {
-        $this->oil=$oil;
+        $this->oil = $oil;
     }
+
     public function getOil()
     {
         return $this->oil;
     }
+
+    //Залить бензин
+    public function setOil($oil)
+    {
+        $this->oil = $oil;
+    }
+
     public function nextOil()
     {
-    $this->oil-=1;
+        $this->oil -= 1;
     }
 }
 
 class Car
 {
-private $color;
-private $type;
-private $engine;
+    private $color;
+    private $type;
+    private $engine;
 
-public $kl;
+    public $kl;
 
-    public function __construct($color,$type,$engine)
+    public function __construct($color, $type, $engine)
     {
-         $this->color=$color;
-         $this->type=$type;
-         $this->engine=$engine;
+        $this->color = $color;
+        $this->type = $type;
+        $this->engine = $engine;
     }
+
     public function getEngine()
     {
         return $this->engine;
     }
+
     public function setKl($kl)
     {
         $this->kl = $kl;
     }
+
+    //Обращение к двигателю в заливаем бензин
+    public function setOil($oil)
+    {
+        $this->engine->setOil($oil);
+    }
+
     public function sellOil()
     {
-        if($this->getOil()!=null)
-        {
+        if ($this->engine->getOil() !== 0) {
             $this->engine->nextOil();
             echo "Вы проехали 1 кл";
-        }else echo"Бензин закончисля";
+        } else echo "Бензин закончисля";
     }
 }
-$Ford=new Car("Red","Track",new Engine(5));
+
+$Ford = new Car("Red", "Track", new Engine(5));
+$Ford->sellOil();
+$Ford->sellOil();
+$Ford->sellOil();
+$Ford->sellOil();
+$Ford->sellOil();
+$Ford->sellOil();
+$Ford->setOil(2);
+$Ford->sellOil();
 $Ford->sellOil();
 var_dump($Ford);
