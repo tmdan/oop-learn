0cfafe48a58c1c7836888ed54a00fe28d019199e
Яна <andreiocsana@gmail.com>
2020-04-12 17:10:15 +0300

    Домашка 40-3

--- yana/40-task-3/index.php
+++ yana/40-task-3/index.php
@@ -1,1 +1,61 @@
 <?php
+
+
+class SpaceShip
+{
+    private $name;
+    private $speed;
+    private $planet;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getSpeed()
+    {
+        return $this->speed;
+    }
+
+    public function __construct($name, $speed, $planet)
+    {
+        $this->name=$name;
+        $this->speed=$speed;
+        $this->planet=$planet;
+    }
+
+    public function getTime()
+    {
+        $time=0;
+        $time=$this->planet->getDistance() / $this->speed;
+        return $time;
+    }
+
+}
+
+class Planet
+{
+    private $name;
+    private $distance;
+
+    public function getNamePl()
+    {
+        return $this->name;
+    }
+
+    public function getDistance()
+    {
+        return $this->distance;
+    }
+
+    public function __construct($name, $distance)
+    {
+        $this->name=$name;
+        $this->distance=$distance;
+    }
+}
+
+
+$Enterprice = new SpaceShip("Энтерпрайс", 50000, new Planet("Луна", 384400));
+
+var_dump($Enterprice->getTime());
